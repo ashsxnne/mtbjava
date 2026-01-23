f9698d1d4f7eabbf9f7ad56acfc1c6d1225a915b
ashsxnne <jaylordlaspuna1@gmail.com>
2026-01-23 14:55:17 +0800

    updatednew2

--- src/Main/landingpage.java
+++ src/Main/landingpage.java
@@ -33,6 +33,10 @@ public class landingpage extends BaseFrame {
         movie2 = new javax.swing.JLabel();
         movie3 = new javax.swing.JLabel();
         movie4 = new javax.swing.JLabel();
+        jToggleButton1 = new javax.swing.JToggleButton();
+        jToggleButton2 = new javax.swing.JToggleButton();
+        jToggleButton3 = new javax.swing.JToggleButton();
+        jToggleButton4 = new javax.swing.JToggleButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -53,7 +57,7 @@ public class landingpage extends BaseFrame {
         jPanel2.add(seachbutton, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 30, 120, 40));
 
         login.setBackground(new java.awt.Color(34, 61, 114));
-        login.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        login.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         login.setForeground(new java.awt.Color(255, 255, 255));
         login.setText("Login");
         login.addActionListener(new java.awt.event.ActionListener() {
@@ -64,6 +68,7 @@ public class landingpage extends BaseFrame {
         jPanel2.add(login, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 30, 110, 50));
 
         signup.setBackground(new java.awt.Color(34, 61, 114));
+        signup.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         signup.setForeground(new java.awt.Color(255, 255, 255));
         signup.setText("Sign Up");
         signup.addActionListener(new java.awt.event.ActionListener() {
@@ -118,7 +123,7 @@ public class landingpage extends BaseFrame {
         jPanel1.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 520, 960, 60));
 
         movie1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/movies/1.jpg"))); // NOI18N
-        jPanel1.add(movie1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 180, 210, 320));
+        jPanel1.add(movie1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 180, 210, 270));
 
         jLabel6.setBackground(new java.awt.Color(216, 184, 86));
         jLabel6.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
@@ -138,15 +143,59 @@ public class landingpage extends BaseFrame {
 
         movie2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/movies/2.png"))); // NOI18N
         movie2.setText("jLabel2");
-        jPanel1.add(movie2, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 180, 210, 320));
+        jPanel1.add(movie2, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 180, 220, 270));
 
         movie3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/movies/3.jpg"))); // NOI18N
         movie3.setText("jLabel2");
-        jPanel1.add(movie3, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 180, 210, 320));
+        jPanel1.add(movie3, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 180, 190, 270));
 
         movie4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/movies/4.png"))); // NOI18N
         movie4.setText("jLabel2");
-        jPanel1.add(movie4, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 180, 210, 320));
+        jPanel1.add(movie4, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 180, 210, 270));
+
+        jToggleButton1.setBackground(new java.awt.Color(255, 255, 255));
+        jToggleButton1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jToggleButton1.setForeground(new java.awt.Color(34, 47, 73));
+        jToggleButton1.setText("Buy Tickets");
+        jToggleButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jToggleButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 460, 140, 40));
+
+        jToggleButton2.setBackground(new java.awt.Color(255, 255, 255));
+        jToggleButton2.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jToggleButton2.setForeground(new java.awt.Color(34, 47, 73));
+        jToggleButton2.setText("Buy Tickets");
+        jToggleButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton2ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jToggleButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 460, 140, 40));
+
+        jToggleButton3.setBackground(new java.awt.Color(255, 255, 255));
+        jToggleButton3.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jToggleButton3.setForeground(new java.awt.Color(34, 47, 73));
+        jToggleButton3.setText("Buy Tickets");
+        jToggleButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton3ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jToggleButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 460, 140, 40));
+
+        jToggleButton4.setBackground(new java.awt.Color(255, 255, 255));
+        jToggleButton4.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jToggleButton4.setForeground(new java.awt.Color(34, 47, 73));
+        jToggleButton4.setText("Buy Tickets");
+        jToggleButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleButton4ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jToggleButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 460, 140, 40));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -156,7 +205,7 @@ public class landingpage extends BaseFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 580, Short.MAX_VALUE)
         );
 
         pack();
@@ -215,6 +264,126 @@ public class landingpage extends BaseFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_seachbuttonActionPerformed
 
+    private void jToggleButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton3ActionPerformed
+        String message = "To book tickets, please sign in or create an account.";
+        String title = "Authentication Required";
+
+        String[] options = {"Login", "Sign Up", "Cancel"};
+
+        int choice = javax.swing.JOptionPane.showOptionDialog(
+                this,
+                message,
+                title,
+                javax.swing.JOptionPane.YES_NO_CANCEL_OPTION,
+                javax.swing.JOptionPane.INFORMATION_MESSAGE,
+                null,
+                options,
+                options[0]
+        );
+
+        if (choice == 0) { // Login
+            Login loginFrame = new Login();
+            loginFrame.setVisible(true);
+            this.dispose();
+
+        } else if (choice == 1) { // Sign Up
+            SignUp signUpFrame = new SignUp();
+            signUpFrame.setVisible(true);
+            this.dispose();
+        }
+        // Cancel = do nothing
+    }//GEN-LAST:event_jToggleButton3ActionPerformed
+
+    private void jToggleButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
+        String message = "To book tickets, please sign in or create an account.";
+        String title = "Authentication Required";
+
+        String[] options = {"Login", "Sign Up", "Cancel"};
+
+        int choice = javax.swing.JOptionPane.showOptionDialog(
+                this,
+                message,
+                title,
+                javax.swing.JOptionPane.YES_NO_CANCEL_OPTION,
+                javax.swing.JOptionPane.INFORMATION_MESSAGE,
+                null,
+                options,
+                options[0]
+        );
+
+        if (choice == 0) { // Login
+            Login loginFrame = new Login();
+            loginFrame.setVisible(true);
+            this.dispose();
+
+        } else if (choice == 1) { // Sign Up
+            SignUp signUpFrame = new SignUp();
+            signUpFrame.setVisible(true);
+            this.dispose();
+        }
+        // Cancel = do nothing
+    }//GEN-LAST:event_jToggleButton1ActionPerformed
+
+    private void jToggleButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton4ActionPerformed
+        String message = "To book tickets, please sign in or create an account.";
+        String title = "Authentication Required";
+
+        String[] options = {"Login", "Sign Up", "Cancel"};
+
+        int choice = javax.swing.JOptionPane.showOptionDialog(
+                this,
+                message,
+                title,
+                javax.swing.JOptionPane.YES_NO_CANCEL_OPTION,
+                javax.swing.JOptionPane.INFORMATION_MESSAGE,
+                null,
+                options,
+                options[0]
+        );
+
+        if (choice == 0) { // Login
+            Login loginFrame = new Login();
+            loginFrame.setVisible(true);
+            this.dispose();
+
+        } else if (choice == 1) { // Sign Up
+            SignUp signUpFrame = new SignUp();
+            signUpFrame.setVisible(true);
+            this.dispose();
+        }
+        // Cancel = do nothing
+    }//GEN-LAST:event_jToggleButton4ActionPerformed
+
+    private void jToggleButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton2ActionPerformed
+        String message = "To book tickets, please sign in or create an account.";
+        String title = "Authentication Required";
+
+        String[] options = {"Login", "Sign Up", "Cancel"};
+
+        int choice = javax.swing.JOptionPane.showOptionDialog(
+                this,
+                message,
+                title,
+                javax.swing.JOptionPane.YES_NO_CANCEL_OPTION,
+                javax.swing.JOptionPane.INFORMATION_MESSAGE,
+                null,
+                options,
+                options[0]
+        );
+
+        if (choice == 0) { // Login
+            Login loginFrame = new Login();
+            loginFrame.setVisible(true);
+            this.dispose();
+
+        } else if (choice == 1) { // Sign Up
+            SignUp signUpFrame = new SignUp();
+            signUpFrame.setVisible(true);
+            this.dispose();
+        }
+        // Cancel = do nothing
+    }//GEN-LAST:event_jToggleButton2ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel contactus;
@@ -224,6 +393,10 @@ public class landingpage extends BaseFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
+    private javax.swing.JToggleButton jToggleButton1;
+    private javax.swing.JToggleButton jToggleButton2;
+    private javax.swing.JToggleButton jToggleButton3;
+    private javax.swing.JToggleButton jToggleButton4;
     private javax.swing.JButton login;
     private javax.swing.JLabel movie1;
     private javax.swing.JLabel movie2;
